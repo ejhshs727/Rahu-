40cfc12aec94cd65b1ed9fa8aeeab15761c5e47d
Rahubby <bbzraaht@gmail.com>
2026-03-25 19:39:58 +0600

    Update info.js

--- Script/commands/info.js
+++ Script/commands/info.js
@@ -22,23 +22,23 @@ module.exports.run = async function({ api, event }) {
 ┏━━━━━━━━━━━━━━━━┓
 ┃  🌟𝐎𝐖𝐍𝐄𝐑 𝐈𝐍𝐅𝐎🌟
 ┣━━━━━━━━━━━━━━━━┫
-┃👤𝐍𝐀𝐌𝐄      :𝐑𝐀𝐇𝐀𝐓
+┃👤𝐍𝐀𝐌𝐄      :𝐑𝐀𝐇𝐔 𝐁𝐁𝐙🤍
 ┃🚹𝐆𝐄𝐍𝐃𝐄𝐑    :𝐌𝐀𝐋𝐄
-┃🎂𝐀𝐆𝐄       :𝟏𝟔
+┃🎂𝐀𝐆𝐄       :18
 ┃🕌𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍 : 𝐈𝐒𝐋𝐀𝐌
-┃🏫𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍 :বয়ড়া ইসরাইল
-┃🏡𝐀𝐃𝐃𝐑𝐄𝐒𝐒 :জামালপুর,বাংলাদেশ
+┃🏫𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍 :𝐈𝐍𝐓𝐄𝐑 2𝐍𝐃 𝐘𝐄𝐀𝐑
+┃🏡𝐀𝐃𝐃𝐑𝐄𝐒𝐒 :𝐌𝐘𝐌𝐄𝐍𝐒𝐈𝐍𝐆 𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇
 ┣━━━━━━━━━━━━━━━━┫
-┃𝐓𝐈𝐊𝐓𝐎𝐊 :@where.is.she15
-┃📢𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌 :দিবো না🥴🤪
-┃🌐𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 :বায়ো-তে আছে
+┃𝐓𝐈𝐊𝐓𝐎𝐊 :@𝐲𝐨𝐮𝐫 𝐁𝐛𝐳 𝐫𝐚𝐡𝐮
+┃📢𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌 :𝐑𝐀𝐇𝐔 𝐁𝐁𝐙
+┃🌐𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 :𝐀𝐁𝐈𝐑 𝐇𝐀𝐒𝐀𝐍 𝐑𝐀𝐇𝐀𝐓
 ┣━━━━━━━━━━━━━━━━┫
 ┃🕒𝐔𝐏𝐃𝐀𝐓𝐄𝐃 𝐓𝐈𝐌𝐄 :${time}
 ┗━━━━━━━━━━━━━━━━┛`,
  attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
 
- return request("https://i.imgur.com/FJI61jS.jpeg")
+ return request("https://i.imgur.com/o5JPIBz.jpeg")
  .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
  .on('close', () => callback());
 };
